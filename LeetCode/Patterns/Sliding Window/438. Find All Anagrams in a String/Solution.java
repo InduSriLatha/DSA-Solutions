fb66e4ac10e83d601a849af0476af42010e54f58
PAKA INDU SRI LATHA <23A81A05A7@sves.org.in>
2026-08-22 11:10:39 +0530

    Add Java Solution to Patterns/Sliding Window

--- LeetCode/Patterns/Sliding Window/438. Find All Anagrams in a String/Solution.java
+++ LeetCode/Patterns/Sliding Window/438. Find All Anagrams in a String/Solution.java
@@ -1,36 +1,22 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
         List<Integer> res=new ArrayList<>();
-        int l=0, r=0;
-        while(r<s.length())
+        int pFreq[]=new int[26];
+        int wFreq[]=new int[26];
+        for(char c:p.toCharArray())
+            pFreq[c-'a']++;
+        int l=0;
+        for(int r=0;r<s.length();r++)
         {
-            if(r-l+1==p.length())
+            wFreq[s.charAt(r)-'a']++;
+            if(r-l+1>p.length())
             {
-                if(isAnagram(s.substring(l,r+1),p))
-                    res.add(l);
+                wFreq[s.charAt(l)-'a']--;
                 l++;
             }
-            r++;
+            if(r-l+1==p.length() && Arrays.equals(pFreq,wFreq))
+                res.add(l);
         }
         return res;
-    }
-    static boolean isAnagram(String s, String p)
-    {
-        HashMap<Character, Integer> hm=new HashMap<>();
-        for(char c: s.toCharArray())
-        {
-            hm.put(c,hm.getOrDefault(c,0)+1);
-        }
-        for(char c: p.toCharArray())
-        {
-            if(!hm.containsKey(c))
-                return false;
-            hm.put(c, hm.get(c)-1);
-            if(hm.get(c)==0)
-                hm.remove(c);
-        }
-        if(hm.size()==0)
-            return true;
-        return false;
     }
 }
