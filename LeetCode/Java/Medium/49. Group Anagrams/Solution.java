aafa7456af0ede09a11f49dd15fbc07f3c9ebd74
PAKA INDU SRI LATHA <23A81A05A7@sves.org.in>
2026-08-22 12:45:29 +0530

    Add Medium Java Solution from LeetCode

---
+++ LeetCode/Java/Medium/49. Group Anagrams/Solution.java
@@ -0,0 +1,25 @@
+import java.util.Collections;
+class Solution {
+    
+    public List<List<String>> groupAnagrams(String[] strs) {
+
+        HashMap<String,List<String>> hm=new HashMap<>();
+
+        for(String s: strs)
+        {
+            char a[]=s.toCharArray();
+            Arrays.sort(a);
+            String st=new String(a);
+            if(hm.containsKey(st))
+                hm.get(st).add(s);
+            else
+            {
+                List<String> al=new ArrayList<>();
+                al.add(s);
+                hm.put(st, al);
+            }
+        }
+        return new ArrayList<>(hm.values());
+
+    }
+}
