4f71cc7ba72c3267728d09091bea0da37be75c44
PAKA INDU SRI LATHA <23A81A05A7@sves.org.in>
2026-08-25 12:19:05 +0530

    Add Medium Java Solution from LeetCode

---
+++ LeetCode/Java/Medium/3018. Make String a Subsequence Using Cyclic Increments/Solution.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean canMakeSubsequence(String str1, String str2) {
+
+        int n1=str1.length();
+        int n2=str2.length();
+        if(n1<n2)
+            return false;
+        
+        int i=0, j=0;
+        while(i<n1 && j<n2)
+        {
+            char ch1=str1.charAt(i);
+            char ch2=str2.charAt(j);
+            if(ch1==ch2)
+            {
+                i++;
+                j++;
+            }
+            else if(ch1+1==ch2 || (ch1=='z' && ch2=='a'))
+            {
+                i++;
+                j++;
+            }
+            else
+                i++;
+        }
+        if(j==n2)
+            return true;
+        return false;
+    }
+}
