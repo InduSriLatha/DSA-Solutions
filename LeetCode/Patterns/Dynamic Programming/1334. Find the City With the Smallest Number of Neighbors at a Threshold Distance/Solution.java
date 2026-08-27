68bf76380b714e504f31b317885057e893108bf2
PAKA INDU SRI LATHA <23A81A05A7@sves.org.in>
2026-08-27 22:14:08 +0530

    Add Medium Java Solution for 1334. Find the City With the Smallest Number of Neighbors at a Threshold Distance from LeetCode

---
+++ LeetCode/Patterns/Dynamic Programming/1334. Find the City With the Smallest Number of Neighbors at a Threshold Distance/Solution.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int findTheCity(int n, int[][] edges, int distanceThreshold) {
+        int dist[][]=new int[n][n];
+        for(int[] row:dist)
+        {
+            Arrays.fill(row,10001);
+        }
+        for(int e[]:edges)
+        {
+            dist[e[0]][e[1]]=dist[e[1]][e[0]]=e[2];
+        }
+        for(int i=0;i<n;i++)
+        {
+            dist[i][i]=0;
+        }
+        for(int k=0;k<n;k++)
+        {
+            for(int i=0;i<n;i++)
+            {
+                for(int j=0;j<n;j++)
+                {
+                    dist[i][j]=Math.min(dist[i][j],dist[i][k]+dist[k][j]);
+                }
+            }
+        }
+        int res=0, smallest=n;
+        for(int i=0;i<n;i++)
+        {
+            int count=0;
+            for(int j=0;j<n;j++)
+            {
+                if(dist[i][j]<=distanceThreshold)
+                    count++;
+            }
+            if(count<=smallest)
+            {
+                res=i;
+                smallest=count;
+            }
+        }
+        return res;
+    }
+}
