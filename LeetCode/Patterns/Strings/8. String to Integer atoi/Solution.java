fa526a599fe78628259c592027148d87b116dd97
PAKA INDU SRI LATHA <23A81A05A7@sves.org.in>
2026-09-09 15:21:01 +0530

    Add Medium Java Solution for 8. String to Integer (atoi) from LeetCode

--- LeetCode/Patterns/Strings/8. String to Integer atoi/Solution.java
+++ LeetCode/Patterns/Strings/8. String to Integer atoi/Solution.java
@@ -1,29 +1,29 @@
 class Solution {
     public int myAtoi(String s) {
-        int i=0;
-        int len=s.length();
+        int i=0, len=s.length();
         while(i<len && s.charAt(i)==' ')
             i++;
-        int sign =1;
-        if(i<len && (s.charAt(i)=='+'|| s.charAt(i)=='-'))
+        int sign=1;
+        if(i<len && (s.charAt(i)=='+' || s.charAt(i)=='-'))
         {
             sign=(s.charAt(i)=='+')?1:-1;
             i++;
         }
         while(i<len && s.charAt(i)=='0')
             i++;
-        return helper(sign,0,s,i);
+        return helper(i,s,0,sign);
+        
     }
-    public int helper(int sign, long num, String s, int i)
+    public int helper(int i, String s, long num, int sign)
     {
-        final int min_val= -2147483648;
-        final int max_val = 2147483647;
+        final int min_value=Integer.MIN_VALUE;
+        final int max_value=Integer.MAX_VALUE;
 
         if(i>=s.length() || !Character.isDigit(s.charAt(i)))
             return (int)(num*sign);
         num=num*10+(s.charAt(i)-'0');
-        if(num*sign<min_val)    return min_val;
-        if(num*sign>max_val)    return max_val;
-        return helper(sign, num,s,i+1);
+        if(num*sign<min_value)   return min_value;
+        if(num*sign>max_value)   return max_value;
+        return helper(i+1,s,num,sign);
     }
 }
