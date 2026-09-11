e9994604b78a4f6ed64e9242528e0d9c90ed18f7
PAKA INDU SRI LATHA <23A81A05A7@sves.org.in>
2026-09-11 10:37:24 +0530

    Add Easy Java Solution for 643. Maximum Average Subarray I from LeetCode

--- LeetCode/Java/Easy/643. Maximum Average Subarray I/Solution.java
+++ LeetCode/Java/Easy/643. Maximum Average Subarray I/Solution.java
@@ -1,17 +1,19 @@
 class Solution {
     public double findMaxAverage(int[] nums, int k) {
-        int l=0, r=0, n=nums.length;
-        double res=0.0, sum=0.0;
-        while(r<n)
+        if(k>nums.length)
+            return -1;
+        int l=0, r=0;
+        int sum=0;
+        double res=-Double.MAX_VALUE;
+        while(r<nums.length)
         {
             sum=sum+nums[r];
-            if(r-l+1>k)
+            if(r-l+1==k)
             {
+                res=Math.max(res,(double)sum/k);
                 sum=sum-nums[l];
                 l++;
             }
-            if(r-l+1==k)
-                res=Math.max(res,sum/k);
             r++;
         }
         return res;
