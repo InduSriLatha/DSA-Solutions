2b907346cccf78f76c390ce62ee502f42e77eeaa
PAKA INDU SRI LATHA <23A81A05A7@sves.org.in>
2026-09-11 09:06:04 +0530

    Add Medium Java Solution for 155. Min Stack from LeetCode

--- LeetCode/Patterns/Stack & Queue/155. Min Stack/Solution.java
+++ LeetCode/Patterns/Stack & Queue/155. Min Stack/Solution.java
@@ -1,42 +1,36 @@
 class MinStack {
-
-    Stack<Integer> s;
-    Stack<Integer> min;
+    Stack<Integer> st;
+    List<Integer> min;
     public MinStack() {
-        s = new Stack<>();
-        min = new Stack<>();
+        st=new Stack<>();
+        min=new ArrayList<>();
     }
-    public void push(int val) {
-        s.push(val);
-
-        if (min.isEmpty() || val <= min.peek()) {
-            min.push(val);
-        }
+    
+    public void push(int value) {
+        if(min.size()>0 && value>min.get(min.size()-1))
+            min.add(min.get(min.size()-1));
+        else
+            min.add(value); 
+        st.push(value);
     }
     
     public void pop() {
-        if (!s.isEmpty()) {
-            int removed = s.pop();
-
-            if (removed == min.peek()) {
-                min.pop();
-            }
-        }
+        st.pop();
     }
     
     public int top() {
-        return s.peek();
+        return st.peek();
     }
     
     public int getMin() {
-        return min.peek();
+        return min.get(st.size()-1);   
     }
 }
 
 /**
  * Your MinStack object will be instantiated and called as such:
  * MinStack obj = new MinStack();
- * obj.push(val);
+ * obj.push(value);
  * obj.pop();
  * int param_3 = obj.top();
  * int param_4 = obj.getMin();
