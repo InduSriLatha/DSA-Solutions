bb78a5330515d892b085c1247d23027ce31aac15
PAKA INDU SRI LATHA <23A81A05A7@sves.org.in>
2026-09-11 09:15:45 +0530

    Add Medium Java Solution for 155. Min Stack from LeetCode

--- LeetCode/Java/Medium/155. Min Stack/README.md
+++ LeetCode/Java/Medium/155. Min Stack/README.md
@@ -8,8 +8,8 @@
 Stack, Design
 
 ### 🚀 Performance
-- **Runtime:** 31 ms
-- **Memory:** 99.6 MB
+- **Runtime:** 5 ms
+- **Memory:** 47.3 MB
 
 ---
 
--- LeetCode/Java/Medium/155. Min Stack/Solution.java
+++ LeetCode/Java/Medium/155. Min Stack/Solution.java
@@ -1,39 +1,42 @@
 class MinStack {
-    Stack<Integer> st;
+
+    Stack<Integer> s;
     Stack<Integer> min;
     public MinStack() {
-        st=new Stack<>();
-        min=new Stack<>();
+        s = new Stack<>();
+        min = new Stack<>();
     }
-    
-    public void push(int value) {
-        st.push(value);
-        if(min.isEmpty() || value<=min.peek())
-            min.push(value);
+    public void push(int val) {
+        s.push(val);
+
+        if (min.isEmpty() || val <= min.peek()) {
+            min.push(val);
+        }
     }
     
     public void pop() {
-        if(!st.isEmpty())
-        {
-            int removed=st.pop();
-            if(removed==min.peek())
+        if (!s.isEmpty()) {
+            int removed = s.pop();
+
+            if (removed == min.peek()) {
                 min.pop();
+            }
         }
     }
     
     public int top() {
-        return st.peek();
+        return s.peek();
     }
     
     public int getMin() {
-        return min.peek();   
+        return min.peek();
     }
 }
 
 /**
  * Your MinStack object will be instantiated and called as such:
  * MinStack obj = new MinStack();
- * obj.push(value);
+ * obj.push(val);
  * obj.pop();
  * int param_3 = obj.top();
  * int param_4 = obj.getMin();
