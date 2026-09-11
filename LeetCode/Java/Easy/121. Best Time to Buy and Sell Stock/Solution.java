1a566ebf130fabacec5ff06acadf7e089f137363
PAKA INDU SRI LATHA <23A81A05A7@sves.org.in>
2026-09-11 12:47:12 +0530

    Add Easy Java Solution for 121. Best Time to Buy and Sell Stock from LeetCode

--- LeetCode/Java/Easy/121. Best Time to Buy and Sell Stock/Solution.java
+++ LeetCode/Java/Easy/121. Best Time to Buy and Sell Stock/Solution.java
@@ -1,16 +1,20 @@
 class Solution {
     public int maxProfit(int[] prices) {
+
         int n=prices.length;
-        int maxsofar=prices[n-1],maxprofit=0;
-		for(int i=n-2;i>=0;i--)
-		{
-			if(prices[i]>maxsofar)
-			{
-				maxsofar=prices[i];
-			}
-			if(maxsofar-prices[i]>maxprofit)
-				maxprofit=maxsofar-prices[i];
-		}
-		return maxprofit;
+        if(n<=1)
+            return 0;
+        int max=prices[n-1];
+        int res=0;
+        for(int i=n-2;i>=0;i--)
+        {
+            if(max-prices[i]>res)
+            {
+                res=max-prices[i];
+            }
+            if(prices[i]>max)
+                max=prices[i];
+        }
+        return res;
     }
 }
