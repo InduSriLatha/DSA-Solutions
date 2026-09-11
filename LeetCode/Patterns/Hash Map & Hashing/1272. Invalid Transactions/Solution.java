7710d9e26595c852367cbee3abab54f062b13c6f
PAKA INDU SRI LATHA <23A81A05A7@sves.org.in>
2026-09-11 13:31:22 +0530

    Add Medium Java Solution for 1272. Invalid Transactions from LeetCode

--- LeetCode/Patterns/Hash Map & Hashing/1272. Invalid Transactions/Solution.java
+++ LeetCode/Patterns/Hash Map & Hashing/1272. Invalid Transactions/Solution.java
@@ -1,45 +1,35 @@
 class Solution {
     public List<String> invalidTransactions(String[] transactions) {
-        List<String> res=new ArrayList<>();
-
-        List<List<String>> transac=new ArrayList<>();
-        for(String s:transactions)
+        List<String[]> transac=new ArrayList<>();
+        for(String s: transactions)
         {
-            List<String> t=Arrays.asList(s.split(","));
-            transac.add(t);
+            String w[]=s.split(",");
+            transac.add(w);
         }
-        List<Integer> ind=new ArrayList<>();
+        List<Integer> idx=new ArrayList<>();
+        List<String> invalid=new ArrayList<>();
         for(int i=0;i<transac.size();i++)
         {
-            if(ind.contains(i))
-                continue;
-            if(Integer.parseInt(transac.get(i).get(2))>1000)
+            String a[]=transac.get(i);
+            if(Integer.parseInt(a[2])>1000 && !idx.contains(i))
             {
-                res.add(transactions[i]);
-                ind.add(i);
-            }
-            for(int j=0;j<transac.size();j++)
-            {
-                if(transac.get(i).get(0).equals(transac.get(j).get(0)) && i!=j)
-                {
-                    int d1=Integer.parseInt(transac.get(i).get(1));
-                    int d2=Integer.parseInt(transac.get(j).get(1));
-                    if(Math.abs(d1-d2)<=60 && !transac.get(i).get(3).equals(transac.get(j).get(3)))
-                    {
-                        if(!ind.contains(i))
-                        {
-                            res.add(transactions[i]);
-                            ind.add(i);
-                        }
-                        if(!ind.contains(j))
-                        {
-                            res.add(transactions[j]);
-                            ind.add(j);
-                        }
-                    }
-                }
+                idx.add(i);
+                invalid.add(transactions[i]);
             }
         }
-        return res;
+        for(int i=0;i<transac.size()-1;i++)
+        {
+            String a1[]=transac.get(i);
+            String a2[]=transac.get(i+1);
+            int d=Math.abs(Integer.parseInt(a1[1])-Integer.parseInt(a2[1]));
+            if(d<=60 && a1[0].equals(a2[0]) && !a1[3].equals(a2[3]))
+            {
+                if(!idx.contains(i))
+                    invalid.add(transactions[i]);
+                if(!idx.contains(i+1))
+                    invalid.add(transactions[i+1]);
+            }
+        }
+        return invalid;
     }
 }
