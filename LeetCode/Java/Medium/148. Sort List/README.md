3cd3c0657a58e8fb679aed086e62aef0c872b810
PAKA INDU SRI LATHA <23A81A05A7@sves.org.in>
2026-09-15 20:01:23 +0530

    Add Medium Java Solution for 148. Sort List from LeetCode

---
+++ LeetCode/Java/Medium/148. Sort List/README.md
@@ -0,0 +1,16 @@
+# 📝 148. Sort List (LeetCode)
+
+🔗 [Problem Link](https://leetcode.com/problems/sort-list/)
+
+![Difficulty](https://img.shields.io/badge/Difficulty-Medium-orange) ![Language](https://img.shields.io/badge/Language-Java-blue)
+
+### 💡 Tags
+Linked List, Two Pointers, Divide and Conquer, Sorting, Merge Sort
+
+### 🚀 Performance
+- **Runtime:** N/A
+- **Memory:** N/A
+
+---
+
+*Description omitted by preference*
---
+++ LeetCode/Java/Medium/148. Sort List/Solution.java
@@ -0,0 +1,69 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode sortList(ListNode head) {
+        if(head==null || head.next==null)
+            return head;
+        ListNode mid=findMid(head);
+        ListNode righthead=mid.next;
+        mid.next=null;
+        ListNode left=sortList(head);
+        ListNode right=sortList(righthead);
+        return merge(left, right);
+    }
+    public static ListNode findMid(ListNode head)
+    {
+        if(head == null || head.next == null){
+            return head;
+        }
+        ListNode sp=head, fp=head;
+        fp=fp.next;
+        while(fp!=null && fp.next!=null)
+        {
+            sp=sp.next;
+            fp=fp.next.next;
+        }
+        return sp;
+    }
+    public static ListNode merge(ListNode head1, ListNode head2)
+    {
+        ListNode m=new ListNode(-1);
+        ListNode temp=m;
+
+        while(head1!=null && head2!=null)
+        {
+            if(head1.val<=head2.val)
+            {
+                temp.next=head1;
+                head1=head1.next;
+            }
+            else
+            {
+                temp.next=head2;
+                head2=head2.next;
+            }
+            temp=temp.next;
+        }
+        while(head1!=null)
+        {
+            temp.next=head1;
+            head1=head1.next;
+            temp=temp.next;
+        }
+        while(head2!=null)
+        {
+            temp.next=head2;
+            head2=head2.next;
+            temp=temp.next;
+        }
+        return m.next;
+    }
+}
