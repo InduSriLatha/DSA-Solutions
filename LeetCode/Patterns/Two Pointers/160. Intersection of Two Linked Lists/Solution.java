6c10190cdf38d2e7117850498a73f25947366e61
PAKA INDU SRI LATHA <23A81A05A7@sves.org.in>
2026-09-15 20:15:55 +0530

    Add Easy Java Solution for 160. Intersection of Two Linked Lists from LeetCode

--- LeetCode/Patterns/Two Pointers/160. Intersection of Two Linked Lists/Solution.java
+++ LeetCode/Patterns/Two Pointers/160. Intersection of Two Linked Lists/Solution.java
@@ -14,14 +14,15 @@ public class Solution {
         ListNode temp1=headA, temp2=headB;
         while(temp1!=temp2)
         {
-            temp1=temp1.next;
-            temp2=temp2.next;
-            if(temp1==temp2)    return temp1;
-            if(temp1==null)
+            if(temp1!=null)
+                temp1=temp1.next;
+            else
                 temp1=headB;
-            if(temp2==null)
+            if(temp2!=null)
+                temp2=temp2.next;
+            else
                 temp2=headA;
         }
-        return null;
+        return temp1;
     }
 }
