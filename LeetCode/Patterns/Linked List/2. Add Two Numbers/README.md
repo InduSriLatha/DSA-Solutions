3173e3cc0a0af86f0b623bf7296460e1cae2a5bb
PAKA INDU SRI LATHA <23A81A05A7@sves.org.in>
2026-09-15 21:08:01 +0530

    Add Medium Java Solution for 2. Add Two Numbers from LeetCode

--- LeetCode/Patterns/Linked List/2. Add Two Numbers/README.md
+++ LeetCode/Patterns/Linked List/2. Add Two Numbers/README.md
@@ -8,8 +8,8 @@
 Linked List, Math, Recursion
 
 ### 🚀 Performance
-- **Runtime:** 1 ms
-- **Memory:** 44.9 MB
+- **Runtime:** N/A
+- **Memory:** N/A
 
 ---
 
--- LeetCode/Patterns/Linked List/2. Add Two Numbers/Solution.java
+++ LeetCode/Patterns/Linked List/2. Add Two Numbers/Solution.java
@@ -10,26 +10,28 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode dummy = new ListNode(-1);
-        ListNode current = dummy;
-        int carry = 0;
-
-        while (l1 != null || l2 != null || carry != 0) {
-            int sum = carry;
-            if (l1 != null) {
-                sum += l1.val;
-                l1 = l1.next;
+        int sum=0, carry=0;
+        ListNode d=new ListNode(-1);
+        ListNode head=d;
+        while(l1!=null || l2!=null || carry!=0)
+        {
+            if(l1!=null)
+            {
+                sum=sum+l1.val;
+                l1=l1.next;
             }
-            if (l2 != null) {
-                sum += l2.val;
-                l2 = l2.next;
+            if(l2!=null)
+            {
+                sum=sum+l2.val;
+                l2=l2.next;
             }
-
-            current.next = new ListNode(sum % 10);
-            carry = sum / 10;
-            current = current.next;
+            sum=sum+carry;
+            ListNode temp=new ListNode(sum%10);
+            d.next=temp;
+            d=d.next;
+            carry=sum/10;
+            sum=0;
         }
-
-        return dummy.next;
+        return head.next;
     }
 }
